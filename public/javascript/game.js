18e2b0de466389c833b8151cdb438c39e5c632be
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-04-05 21:37:15 -0500

    highscores route fixed

--- public/javascript/game.js
+++
@@ -1,7 +0,0 @@
-const blackjack = require('engine-blackjack-ts')
-const actions = blackjack.actions
-const Game = blackjack.Game
-
-const game = new Game(); // default loaded unless specified
-
-console.dir(game.getState())
