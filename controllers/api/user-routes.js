5a719364dd76c3fb51c64acecd622f082fd5f732
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-04-07 19:11:27 -0500

    heroku updates

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, Score, Comment } = require("../../models");
-const withAuth = require("../../utils/auth");
+//const withAuth = require("../../utils/auth");
 
 // get all users
 // http://localhost:3001/api/users
--- controllers/highscore-routes.js
+++ controllers/highscore-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Score, User } = require("../models");
-const withAuth = require("../utils/auth");
+// const withAuth = require("../utils/auth");
 
 router.get("/", (req, res) => {
   console.log("triggered");
